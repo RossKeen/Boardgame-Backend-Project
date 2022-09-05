3999d1067a66c0749026cdfed531560a16eb97c7
RossKeen <rosssamuelkeen@gmail.com>
2022-09-05 14:28:24 +0100

    add inital error functions. only server errors currently populated

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const { handleCustomErrors, handlePsqlErrors, handleServerErrors } = require("./errors");
 
 const app = express();
+
+app.use(handleCustomErrors);
+app.use(handlePsqlErrors);
+app.use(handleServerErrors);
 
 module.exports = app;
